2d8a4f27ad6cba0877227d583050613be21476a0
Pavan kumar lakshmisetty <lakshmisetty.pavan@gmail.com>
2018-03-04 18:27:31 +0100

    commit
    test

--- Calculator/src/main/java/calc/CalculatorMain.java
+++ Calculator/src/main/java/calc/CalculatorMain.java
@@ -1,10 +1,95 @@
 package calc;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class CalculatorMain {
 	public CalculatorMain(){
-		
+
 	}
 	public int add(int a, int b){
 		return a+b;
 	}
+
+	public int sub(int a, int b){
+		return a-b;
+	}
+
+	public int mul(int a, int b) {
+		return a*b;
+	}
+	public int div(int a, int b){
+		if(b == 0) {
+			return 0;
+		}else {
+			return a/b;
+		}
+	}
+	public double sin(double a) {
+		return Math.sin( Math.toRadians(a));
+	}
+	public double abs(double a) {
+		if (a<0) 
+		{
+			return -a;
+		}
+		else
+		{
+			return a;
+		}
+	}
+	
+	/* Additional Functions */
+	
+	public double squareRoot(double a) {
+		if (a < 0) {
+			return 0;
+		}
+		return Math.sqrt(a);
+	}
+
+	/*Adding new function */
+	double power(int a, int b){
+		double answer =a;
+
+		if(b==0){
+			return 1;
+		}else if(b==1)
+		{
+			return a;
+
+		}else {
+
+			for (int x=2; x<=Math.abs(b); x++){
+				answer *= a;
+			}
+			
+			if(b<0){
+				return 1/answer;
+			}
+			
+		}
+		return answer;
+	}
+	
+	double log(int a, int b) {
+		if(a>0 && a!=1 && b>0 && b!=1) {
+			return Math.log(a)/Math.log(b);
+		}else return 0;	
+	}
+	
+	/* Factorial Function */
+	int factorial(int a) {
+		int answer = 1;
+		for(int x = 2; x <= a;x++) {
+			answer *= x;
+		}
+		return answer;
+	}
+	
+	
+	
+	
+	
 }
--- Calculator/src/test/java/calc/CaculatorTest.java
+++ Calculator/src/test/java/calc/CaculatorTest.java
@@ -14,16 +14,95 @@ public class CaculatorTest {
 	}
 	
 	@Test
-	public void testAdd6() {
+	public void testAdd() {
 		assertEquals(5, calc.add(3,2));
-
+	}
+	@Test
+	public void testSub() {
+		assertEquals(1, calc.sub(3,2));
+	}
+	@Test
+	public void testMul() {
+		assertEquals(6, calc.mul(3,2));
+	}
+	@Test
+	public void testDiv() {
+		assertEquals(1, calc.div(2,2));
+	}
+	@Test
+	public void testDivByZero() {
+		assertEquals(0, calc.div(3,0));
+	}
+	@Test
+	public void testPresedence() {
+		assertEquals(30, calc.add(calc.mul(5, 5), calc.div(10, 2)));
 	}
 	
-		
-@Test
-	public void testAdd6() {
-		assertEquals(6, calc.add(3,3));
-
+	// Test case related to new function
+	@Test
+	public void testSqRt() {
+		assertEquals(4, calc.squareRoot(16), 1e-10);
+	}
+	@Test
+	public void testSqRtNegative() {
+		assertEquals(0, calc.squareRoot(-8),1e-10);
 	}
 	
+	@Test
+	public void testPower() {
+		assertEquals(9, calc.power(3,2),1e-10);
+	}
+	
+	@Test
+	public void testPowerNegative() {
+		assertEquals(0.04, calc.power(-5,-2),1e-10);
+	}
+	
+	@Test
+	public void testPowerzero() {
+		assertEquals(-0.2, calc.power(-5,-1),1e-10);
+	}
+	
+	@Test
+	public void testFactorial() {
+		assertEquals(120, calc.factorial(5));
+	}
+	
+	@Test
+	public void testLog() {
+		assertEquals(0.25, calc.log(2, 16),1e-10);
+	}
+	
+	@Test
+	public void testLogNagative() {
+		assertEquals(0, calc.log(2, -16),1e-10);
+	}
+	@Test 
+	public void testSin() {
+		assertEquals(0.5, calc.sin(30),1e-10);
+	}
+	@Test
+	public void testAbs() {
+		assertEquals(1,calc.abs(-1),1e-10);
+	}
+	
+
+	@Test
+	public void testAddPower() {
+		assertEquals(15, calc.add((int)calc.power(2, 3), calc.sub(9, 2)));
+	}
+	
+	
+
+	// Integration Tests
+	@Test
+	public void testSqRtPower() {
+		assertEquals(100, calc.squareRoot(calc.power(100, 2)),1e-10);
+	}
+	
+	@Test
+	public void testIntegrateAddPowerSub() {
+		assertEquals(15, calc.add((int)calc.power(2, 3), calc.sub(9, 2)));
+	}
+
 }
